86687341c22b3959130053b041d9d34d29f24838
andrewflbarnes <andrew.barnes2@mastercard.com>
2020-03-06 03:15:07 +0000

    Allow cross origin for whatis GET

--- penny-guess-server/src/main/java/com/andrewflbarnes/penny/component/whatis/WhatIsController.java
+++ penny-guess-server/src/main/java/com/andrewflbarnes/penny/component/whatis/WhatIsController.java
@@ -2,6 +2,7 @@ package com.andrewflbarnes.penny.component.whatis;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +23,7 @@ public class WhatIsController {
 
     private WhatIsService whatIsService;
 
+    @CrossOrigin
     @GetMapping
     public List<String> getWhatIs() {
         return whatIsService.getWhatIs();
