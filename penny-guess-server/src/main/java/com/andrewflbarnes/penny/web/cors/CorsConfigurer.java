5f212bec75d9d800b101ecf002fef2639d0f0f23
andrewflbarnes <andrew.barnes2@mastercard.com>
2020-08-17 02:35:00 +0100

    Upgrade Springboot, fix some cors and static resource problems
    - Appropriate cors defaults to prevent NPEs and add nocors profile
    - Removed EnableWebMvc annotation which was preventing spring Web MVC
      config which allowed index.html to be retrieved normally

--- penny-guess-server/src/main/java/com/andrewflbarnes/penny/web/cors/CorsConfigurer.java
+++ penny-guess-server/src/main/java/com/andrewflbarnes/penny/web/cors/CorsConfigurer.java
@@ -5,13 +5,11 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
-@EnableWebMvc
 @Slf4j
 public class CorsConfigurer implements WebMvcConfigurer {
 
--- penny-guess-server/src/main/java/com/andrewflbarnes/penny/web/cors/CorsProperties.java
+++ penny-guess-server/src/main/java/com/andrewflbarnes/penny/web/cors/CorsProperties.java
@@ -3,11 +3,12 @@ package com.andrewflbarnes.penny.web.cors;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @Slf4j
 public class CorsProperties {
-    private List<String> exact;
-    private List<CorsGenerator> generators;
+    private List<String> exact = new ArrayList<>();
+    private List<CorsGenerator> generators = new ArrayList<>();
 }
